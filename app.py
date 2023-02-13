f62ae50ef9afadc57fc0c005f5704f3d5bc1e322
Marlon <61421644+marlon2901@users.noreply.github.com>
2023-02-13 20:33:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,40 +3,39 @@ import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
-server = app.server
 
 # Import data
 df = pd.read_csv("LMSM360_9.csv")
 
 app.layout = html.Div([
+    #Interval
+    dcc.Interval(id = 'my_interval',
+                 disabled = False,
+                 interval = 1 * 4000,
+                 n_intervals = 0,
+                 max_intervals = 4,
+    ),
+    
     html.H1("Lidar Scanner", style = {'text-align': 'center'}),
-    html.A(html.Button('Refresh Data'),href='/'),
-    dcc.Dropdown(id = "Vertical_Increment",
-        options = [
-            {"label": "1.8", "value": 1.8},
-            {"label": "9"  , "value": 9  },
-            {"label": "18" , "value": 18 },
-            {"label": "27" , "value": 27 }],
-        value = 1.8),
-    html.Div(id ='Output_box', children =[]),
     html.Br(),
     dcc.Graph(id ='Scatter', figure = {})])
 
 @app.callback(
-    [Output(component_id = 'Output_box'        , component_property = 'children'),
-     Output(component_id = 'Scatter'           , component_property = 'figure')],
-    [Input (component_id = 'Vertical_Increment', component_property = 'value')]
+    [Output(component_id = 'Scatter'     , component_property = 'figure')],
+    [Input (component_id = 'my_interval' , component_property = 'n_intervals')]
 )
-def update_graph(option_degree):
+def update_graph(num):
     
-    container = "The vertical increment chosen was: {}".format(option_degree)
-    size1 = []
-    for i in range (2433):
-        size1.append(0.000001)
-    fig = px.scatter_3d(df, 
-        x='X', y='Y', z='Z',
-        color="X", size = size1)
-    fig.write_html("3DLidarScannerPlotlyDash.html")
-    return container, fig
+    if num == 0:
+        raise PreventUpdate
+    else:
+        size1 = []
+        for i in range (2433):
+            size1.append(0.000001)
+        fig = px.scatter_3d(df, 
+            x='X', y='Y', z='Z',
+            color="X", size = size1)
+        fig.write_html("3DLidarScannerPlotlyDash.html")
+    return fig
 
-app.run_server(debug=False)
+app.run_server(debug=True)
