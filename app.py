de048f2eea0ac047ce48a4fed7581d251a23c121
Marlon <61421644+marlon2901@users.noreply.github.com>
2023-02-14 20:04:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from dash import Dash, dcc, html, Input, Output
 from dash.exceptions import PreventUpdate
+from dash.dependencies import Output, Input
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
-
-# Import data
-df = pd.read_csv("LMSM360_9.csv")
 
 app.layout = html.Div([
     dcc.Interval( id = 'my_interval',
@@ -26,25 +24,35 @@ app.layout = html.Div([
         value = 1.8),
     html.Div(id ='Output_box', children =[]),
     html.Br(),
+    dcc.Store(id = "store"),
+    html.Div(id = "trigger"),
     dcc.Graph(id ='Scatter', figure = {})])
+
+@app.callback(Output('store','data'), [Input('trigger','children')], prevent_initial_call=False)
+def update_data(children):
+    # Import data
+    df = pd.read_csv("LMSM360_9.csv")
+    return df.to_json()
 
 @app.callback(
     [Output(component_id = 'Output_box'        , component_property = 'children'),
      Output(component_id = 'Scatter'           , component_property = 'figure')],
-    [Input (component_id = 'Vertical_Increment', component_property = 'value'),
-     Input (component_id = 'my_interval'       , component_property = 'n_intervals')]
+    [Input (component_id = 'Vertical_Increment', component_property = 'value')],
+    [State (component_id = 'store'              , component_property = 'data')]
 )
-def update_graph(option_degree, num):
+def update_graph(option_degree, data):
     
     container = "The vertical increment chosen was: {}".format(option_degree)
     rangeLoop = int(df.size/3)
+    print(df.size)
     size1 = []
     for i in range (rangeLoop):
         size1.append(0.000001)
         
-    if num ==0:
+    if data is None:
         raise PreventUpdate
     else:
+        df = pd.read_json(data)
         fig = px.scatter_3d(df, 
         x='X', y='Y', z='Z',
         color="X", size = size1)
@@ -52,3 +60,4 @@ def update_graph(option_degree, num):
     return container, fig
 
 app.run_server(debug=True)
+
