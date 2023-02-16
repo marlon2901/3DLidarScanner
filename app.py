1e6dd46c3f898304a50b1dfa2151848d7f07297c
Marlon <61421644+marlon2901@users.noreply.github.com>
2023-02-16 19:58:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from dash import Dash, dcc, html, Input, Output
-from dash.exceptions import PreventUpdate
-from dash.dependencies import Output, Input
 import plotly.express as px
 import pandas as pd
+
+# Import data 
+df = pd.read_csv('https://raw.githubusercontent.com/marlon2901/3DLidarScanner/main/LMSM360_9.csv')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Interval( id = 'my_interval',
-                  disabled = False,
-                  interval = 1 * 3000,
-                  n_intervals = 0,
-                  max_intervals = 4,
-        ),
-    
     html.H1("Lidar Scanner", style = {'text-align': 'center'}),
     dcc.Dropdown(id = "Vertical_Increment",
         options = [
@@ -24,40 +18,24 @@ app.layout = html.Div([
         value = 1.8),
     html.Div(id ='Output_box', children =[]),
     html.Br(),
-    dcc.Store(id = "store"),
-    html.Div(id = "trigger"),
     dcc.Graph(id ='Scatter', figure = {})])
-
-@app.callback(Output('store','data'), [Input('trigger','children')], prevent_initial_call=False)
-def update_data(children):
-    # Import data
-    df = pd.read_csv("LMSM360_9.csv")
-    return df.to_json()
 
 @app.callback(
     [Output(component_id = 'Output_box'        , component_property = 'children'),
      Output(component_id = 'Scatter'           , component_property = 'figure')],
-    [Input (component_id = 'Vertical_Increment', component_property = 'value')],
-    [State (component_id = 'store'              , component_property = 'data')]
+    [Input (component_id = 'Vertical_Increment', component_property = 'value')]
 )
-def update_graph(option_degree, data):
+def update_graph(option_degree):
     
     container = "The vertical increment chosen was: {}".format(option_degree)
-    rangeLoop = int(df.size/3)
-    print(df.size)
+    rangeLoop = int(df.size /3)
     size1 = []
     for i in range (rangeLoop):
         size1.append(0.000001)
-        
-    if data is None:
-        raise PreventUpdate
-    else:
-        df = pd.read_json(data)
-        fig = px.scatter_3d(df, 
+    fig = px.scatter_3d(df, 
         x='X', y='Y', z='Z',
         color="X", size = size1)
     fig.write_html("3DLidarScannerPlotlyDash.html")
     return container, fig
 
 app.run_server(debug=True)
-
