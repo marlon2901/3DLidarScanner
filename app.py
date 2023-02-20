c9e4da63098c2b1cc56ef7ed4dc1859cc77159b6
Marlon <61421644+marlon2901@users.noreply.github.com>
2023-02-20 12:39:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 from dash import Dash, dcc, html, Input, Output
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Output, Input
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
+# csvPath = "https://raw.githubusercontent.com/marlon2901/3DLidarScanner/main/LMSM360_9.csv"
+csvPath = "LMSM360_9.csv"
+df = pd.read_csv(csvPath)
 
-app.layout = html.Div([
-    html.H1("Lidar Scanner", style = {'text-align': 'center'}),
-    dcc.Dropdown(id = "Vertical_Increment",
-        options = [
-            {"label": "1.8", "value": 1.8},
-            {"label": "9"  , "value": 9  },
-            {"label": "18" , "value": 18 },
-            {"label": "27" , "value": 27 }],
-        value = 1.8),
-    html.Div(id ='Output_box', children =[]),
-    html.Br(),
-    dcc.Graph(id ='Scatter', figure = {})])
+def layout_function():
+    
+    global df
+    df = pd.read_csv(csvPath)
+    print (df)
+    
+    return html.Div([
+        html.H1("Lidar Scanner", style = {'text-align': 'center'}),
+        dcc.Dropdown(
+            id = "Vertical_Increment",
+            options = [
+                {"label": "1.8", "value": 1.8},
+                {"label": "9"  , "value": 9  },
+                {"label": "18" , "value": 18 },
+                {"label": "27" , "value": 27 }],
+                value = 1.8),
+        html.Div(id ='Output_box', children =[]),
+        html.Br(),
+        dcc.Graph(id ='Scatter', figure = {})
+        ])
+
+app.layout = layout_function
 
 @app.callback(
     [Output(component_id = 'Output_box'        , component_property = 'children'),
@@ -24,17 +39,19 @@ app.layout = html.Div([
 )
 def update_graph(option_degree):
     
-    # Import data 
-    df = pd.read_csv('https://raw.githubusercontent.com/marlon2901/3DLidarScanner/main/LMSM360_9.csv')
     container = "The vertical increment chosen was: {}".format(option_degree)
-    rangeLoop = int(df.size /3)
+    rangeLoop = int(df.size/3)
+    print(df.size)
     size1 = []
     for i in range (rangeLoop):
         size1.append(0.000001)
+
     fig = px.scatter_3d(df, 
-        x='X', y='Y', z='Z',
-        color="X", size = size1)
-    #fig.write_html("3DLidarScannerPlotlyDash.html")
+    x='X', y='Y', z='Z',
+    color='X', size = size1)
+    #   fig.write_html("3DLidarScannerPlotlyDash.html")
     return container, fig
 
 app.run_server(debug=True)
+
+
