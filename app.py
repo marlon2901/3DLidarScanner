ed2bdf5ca09bc15d11589dbb9209f71ecde2fcf7
Marlon <61421644+marlon2901@users.noreply.github.com>
2023-02-21 20:21:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 from dash import Dash, dcc, html, Input, Output
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Output, Input
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
-#csvPath = "https://raw.githubusercontent.com/marlon2901/3DLidarScanner/main/LMSM360_9.csv"
-df = pd.read_csv("https://raw.githubusercontent.com/marlon2901/3DLidarScanner/main/LMSM360_9.csv")
 
 def layout_function():
     
     return html.Div([
         html.H1("Lidar Scanner", style = {'text-align': 'center'}),
-        dcc.Dropdown(
-            id = "Vertical_Increment",
-            options = [
-                {"label": "1.8", "value": 1.8},
-                {"label": "9"  , "value": 9  },
-                {"label": "18" , "value": 18 },
-                {"label": "27" , "value": 27 }],
-                value = 1.8),
-        html.Div(id ='Output_box', children =[]),
+        html.Button("Update Graph", id = "UpdateGraph", n_clicks = 0,
+                    style = {'width':'100%','display':'flex',
+                             'align-items':'center',
+                             'justify-content':'center'}),
         html.Br(),
         dcc.Graph(id ='Scatter', figure = {})
         ])
@@ -26,15 +21,12 @@ def layout_function():
 app.layout = layout_function
 
 @app.callback(
-    [Output(component_id = 'Output_box'        , component_property = 'children'),
-     Output(component_id = 'Scatter'           , component_property = 'figure')],
-    [Input (component_id = 'Vertical_Increment', component_property = 'value')]
+    Output(component_id = 'Scatter'    , component_property = 'figure'),
+    Input (component_id = 'UpdateGraph', component_property = 'n_clicks')
 )
-def update_graph(option_degree):
+def update_graph(n_clicks):
     
-    global df
     df = pd.read_csv("https://raw.githubusercontent.com/marlon2901/3DLidarScanner/main/LMSM360_9.csv")
-    container = "The vertical increment chosen was: {}".format(option_degree)
     rangeLoop = int(df.size/3)
     print(df.size)
     size1 = []
@@ -45,8 +37,6 @@ def update_graph(option_degree):
     x='X', y='Y', z='Z',
     color='X', size = size1)
     #   fig.write_html("3DLidarScannerPlotlyDash.html")
-    return container, fig
+    return fig
 
 app.run_server(debug=True)
-
-
